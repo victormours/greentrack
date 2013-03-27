6f9e54f76b89feca1ac9e5ffe4850ba9d2193fd6
Victor Mours <victor.mours@ensimag.imag.fr>
2013-03-27 16:32:34 -0500

    Created app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Greentrack::Application.config.secret_token = '4294c8353160720fba158d3dce8eb5c6056be29d001297efde98597a950f457ec799a93992d3e5b6e346849a5dce1ea0ad05a0b5597cac4b1ac04a4c76fac37e'
